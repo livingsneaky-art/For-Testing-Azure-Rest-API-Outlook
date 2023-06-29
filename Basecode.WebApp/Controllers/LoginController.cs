b430c751c280381a0f5aeebcf072506a1355485e
Jmark <jaymark.carba@cit.edu>
2023-06-29 15:47:03 +0800

    Create login controller file

---
+++ Basecode.WebApp/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
