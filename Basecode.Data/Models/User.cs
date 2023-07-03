851d839c0ce19e15ca4344e4c298c49c24f077e7
kuanhh <hannahbenitez62@gmail.com>
2023-07-03 20:54:41 +0800

    Modify Data Model for User table

--- Basecode.Data/Models/User.cs
+++ Basecode.Data/Models/User.cs
@@ -1,18 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Basecode.Data.Models
 {
     public class User
     {
-        public string Id { get; set; }
-        public string UserID { get; set; }
+        public int Id { get; set; }
+        public string Fullname { get; set; }
         public string Username { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string CreatedBy { get; set; }
-        public DateTime? CreatedDate { get; set; }
-        public string ModifiedBy { get; set; }
-        public DateTime? ModifiedDate { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
     }
 }
