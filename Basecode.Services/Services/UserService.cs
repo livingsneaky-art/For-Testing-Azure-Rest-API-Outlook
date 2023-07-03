77c9002c30393e591ee2296941d87d83f5411d28
kuanhh <hannahbenitez62@gmail.com>
2023-07-03 22:13:09 +0800

    Setup Data, Services, and WebApp for User db connection

--- Basecode.Services/Services/UserService.cs
+++ Basecode.Services/Services/UserService.cs
@@ -1,73 +1,26 @@
 ﻿using Basecode.Data.Interfaces;
 using Basecode.Data.Models;
-using Basecode.Services .Interfaces;
-using Microsoft.AspNetCore.Identity;
+using Basecode.Data.Repositories;
+using Basecode.Services.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Basecode.Services.Services
 {
     public class UserService : IUserService
     {
-        private readonly IUserRepository _userRepository;
-
-        public UserService(IUserRepository userRepository)
+        private readonly IUserRepository _repository;
+        public UserService(IUserRepository repository)
         {
-            _userRepository = userRepository;
+            _repository = repository;
         }
 
-        public User FindByUsername(string username)
+        public List<User> RetrieveAll()
         {
-            return _userRepository.FindByUsername(username);
-        }
-
-        public User FindById(string id)
-        {
-            return _userRepository.FindById(id);
-        }
-
-        public User FindUser(string userName)
-        {
-            return _userRepository.FindUser(userName);
-        }
-
-        public IEnumerable<User> FindAll()
-        {
-            return _userRepository.FindAll();
-        }
-
-        public bool Create(User user)
-        {
-            return _userRepository.Create(user);
-        }
-
-        public bool Update(User user)
-        {
-            return _userRepository.Update(user);
-        }
-
-        public void Delete(User user)
-        {
-            _userRepository.Delete(user);
-        }
-
-        public Task<User> FindUserAsync(string userName, string password)
-        {
-            return _userRepository.FindUserAsync(userName, password);
-        }
-
-        public async Task<IdentityResult> RegisterUser(string username, string password, string firstName, string lastName, string email, string role)
-        {
-            return await _userRepository.RegisterUser(username, password, firstName, lastName, email, role);
-        }
-        public async Task<IdentityResult> CreateRole(string roleName)
-        {
-            return await _userRepository.CreateRole(roleName);
-        }
-
-        public async Task<IdentityUser> FindUser(string username, string password)
-        {
-            return await _userRepository.FindUser(username, password);
+            return _repository.RetrieveAll().ToList();
         }
     }
 }
