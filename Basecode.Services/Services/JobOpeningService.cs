ebfa5b3b2d6758931b5b307023378283521bf22c
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-07-03 10:59:54 +0800

    add connection for jobOpening table

--- Basecode.Services/Services/JobOpeningService.cs
+++ Basecode.Services/Services/JobOpeningService.cs
@@ -4,6 +4,8 @@ using Basecode.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Basecode.Services.Services
 {
