0c1b0d7e8e8b5665d7d8ea68177e8f5f3198ca63
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-07-03 12:19:29 +0800

    add CRUD-create for job opening

--- Basecode.Services/Services/JobOpeningService.cs
+++ Basecode.Services/Services/JobOpeningService.cs
@@ -4,8 +4,6 @@ using Basecode.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Basecode.Services.Services
 {
