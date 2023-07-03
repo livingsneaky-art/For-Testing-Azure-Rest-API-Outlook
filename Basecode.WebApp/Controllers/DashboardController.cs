1f1ec48b68d91ee732afb094eff811caf16287e2
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-07-03 10:17:17 +0800

    make dashboard responsive

--- Basecode.WebApp/Controllers/DashboardController.cs
+++ Basecode.WebApp/Controllers/DashboardController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Basecode.Data.Models;
+using Basecode.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Basecode.WebApp.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly IApplicantService _applicantService;
+
+        public DashboardController(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Applicant> applicants = _applicantService.GetApplicants();
+            return View(applicants);
         }
     }
 }
