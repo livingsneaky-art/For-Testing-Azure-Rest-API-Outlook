d8d0dbbc40b84f19286a331e04895885c933a28f
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-07-03 12:19:29 +0800

    add CRUD-create for job opening

--- Basecode.WebApp/Controllers/JobController.cs
+++ Basecode.WebApp/Controllers/JobController.cs
@@ -1,5 +1,7 @@
 ﻿using Basecode.Services.Interfaces;
+using Basecode.Data.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace Basecode.WebApp.Controllers
 {
@@ -16,6 +18,24 @@ namespace Basecode.WebApp.Controllers
         {
             var jobOpenings = _jobOpeningService.GetJobs();
             return View(jobOpenings);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(JobOpening jobOpening)
+        {
+            if (ModelState.IsValid)
+            {
+                string createdBy = "dummy1";
+                _jobOpeningService.Create(jobOpening, createdBy);
+                return RedirectToAction("Index");
+            }
+
+            return View(jobOpening);
         }
     }
 }
