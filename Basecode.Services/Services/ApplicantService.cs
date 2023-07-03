44f6d94871eb78167ba561a85e3f352dd78c9d0b
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-07-03 10:17:17 +0800

    make dashboard responsive

---
+++ Basecode.Services/Services/ApplicantService.cs
@@ -0,0 +1,27 @@
+﻿using Basecode.Data.Interfaces;
+using Basecode.Data.Models;
+using Basecode.Data.Repositories;
+using Basecode.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Services
+{
+    public class ApplicantService : IApplicantService
+    {
+        private readonly IApplicantRepository _repository;
+
+        public ApplicantService(IApplicantRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Applicant> GetApplicants()
+        {
+            return _repository.GetAll().ToList();
+        }
+    }
+}
