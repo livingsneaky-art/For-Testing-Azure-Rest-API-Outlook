dae84eb811a8d52fe1ddf1c8a2b2b1c9a1cf2819
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-07-03 10:17:17 +0800

    make dashboard responsive

--- Basecode.WebApp/Startup.Dependencies.cs
+++ Basecode.WebApp/Startup.Dependencies.cs
@@ -17,9 +17,11 @@ namespace Basecode.WebApp
 
             // Services 
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IApplicantService, ApplicantService>();
 
             // Repositories
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IApplicantRepository, ApplicantRepository>();
 
         }
     }
