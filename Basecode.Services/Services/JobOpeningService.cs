b52d3efa5e0ac073cbecf00f8ee2aa78255eb628
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-07-03 10:59:54 +0800

    add connection for jobOpening table

---
+++ Basecode.Services/Services/JobOpeningService.cs
@@ -0,0 +1,26 @@
+﻿using Basecode.Data.Interfaces;
+using Basecode.Data.Models;
+using Basecode.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Services
+{
+    public class JobOpeningService : IJobOpeningService
+    {
+        private readonly IJobOpeningRepository _repository;
+
+        public JobOpeningService(IJobOpeningRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<JobOpening> GetApplicants()
+        {
+            return _repository.GetAll().ToList();
+        }
+    }
+}
