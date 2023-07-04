ec11e72ea56f5b795e49b051cef3f61a7959a551
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2023-07-04 16:12:30 +0800

    Add Documentation for Application Tracker

--- Basecode.Data/Interfaces/IApplicationRepository.cs
+++ Basecode.Data/Interfaces/IApplicationRepository.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace Basecode.Data.Interfaces
 {
+    /// <summary>
+    /// Defines methods for managing application data.
+    /// </summary>
     public interface IApplicationRepository
     {
+        /// <summary>
+        /// Retrieves an application by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the application to retrieve.</param>
+        /// <returns>The application with the specified ID, or null if not found.</returns>
         Application GetById(Guid id);
     }
 }
--- Basecode.Services/Interfaces/IApplicationService.cs
+++ Basecode.Services/Interfaces/IApplicationService.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace Basecode.Services.Interfaces
 {
+    /// <summary>
+    /// Defines methods for managing application data.
+    /// </summary>
     public interface IApplicationService
     {
+        /// <summary>
+        /// Retrieves an application by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the application to retrieve.</param>
+        /// <returns>The application with the specified ID, or null if not found.</returns>
         ApplicationViewModel GetById(Guid id);
     }
 }
