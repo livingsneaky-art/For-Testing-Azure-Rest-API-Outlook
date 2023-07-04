0303230310cd1d8d41a1a6699e108d5dde0bfb11
kuanhh <hannahbenitez62@gmail.com>
2023-07-04 16:31:21 +0800

    Add documentation for user service, repository, model

--- Basecode.Data/Models/User.cs
+++ Basecode.Data/Models/User.cs
@@ -6,13 +6,40 @@ using System.Threading.Tasks;
 
 namespace Basecode.Data.Models
 {
+    /// <summary>
+    /// Represents a user entity with properties that store information.
+    /// </summary>
     public class User
     {
+        /// <summary>
+        /// Represents the unique identifier of a user.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Represents the full name of a user.
+        /// </summary>
         public string Fullname { get; set; }
+
+        /// <summary>
+        /// Represents the username associated with a user.
+        /// </summary>
         public string Username { get; set; }
+
+        /// <summary>
+        /// Represents the email address of a user.
+        /// </summary>
         public string Email { get; set; }
+
+        /// <summary>
+        /// Represents the password associated with a user.
+        /// </summary>
         public string Password { get; set; }
+
+        /// <summary>
+        /// Represents the role or position of a user in the system.
+        /// Examples: Human Resources, Deployment Team
+        /// </summary>
         public string Role { get; set; }
     }
 }
--- Basecode.Data/ViewModels/UserViewModel.cs
+++ Basecode.Data/ViewModels/UserViewModel.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Basecode.Data.ViewModels
 {
+    /// <summary>
+    /// Encapsulates data specifically intended for the presentation and interaction requirements of a user interface. 
+    /// Provides a customized representation of user-related information.
+    /// </summary>
     public class UserViewModel
     {
         public int Id { get; set; }
