03ccbf010f11e404d6d98b90be6f4916122c39f3
Brent Lasaca <brent.lasaca@cit.edu>
2023-07-04 12:15:13 +0800

    Transfer input values from personal information to confirmation

--- Basecode.WebApp/Controllers/CharacterReferenceFormController.cs
+++ Basecode.WebApp/Controllers/CharacterReferenceFormController.cs
@@ -4,8 +4,29 @@ namespace Basecode.WebApp.Controllers
 {
     public class CharacterReferenceFormController : Controller
     {
-        public IActionResult Index()
+        [HttpPost]
+        public IActionResult Index(string firstName,
+                                   string middleName,
+                                   string lastName,
+                                   string date,
+                                   string age,
+                                   string gender,
+                                   string nationality,
+                                   string tempStreet,
+                                   string tempCity,
+                                   string tempProvince,
+                                   string tempZip,
+                                   string tempPhone,
+                                   string tempMail)
         {
+            TempData["Name"] = firstName + " " + middleName + " " + lastName;
+            TempData["Birthdate"] = date;
+            TempData["Age"] = age;
+            TempData["Gender"] = gender;
+            TempData["Nationality"] = nationality;
+            TempData["Address"] = tempStreet + ", " + tempCity + ", " + tempProvince + " " + tempZip;
+            TempData["Phone"] = tempPhone;
+            TempData["Email"] = tempMail;
             return View();
         }
     }
---
+++ Basecode.WebApp/Controllers/ConfirmationFormController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class ConfirmationFormController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(string name,
+                                   string birthdate,
+                                   string age,
+                                   string genderFinal,
+                                   string nationalityFinal,
+                                   string address,
+                                   string phone,
+                                   string email)
+        {
+            TempData["Name"] = name;
+            TempData["Birthdate"] = birthdate;
+            TempData["Age"] = age;
+            TempData["Gender"] = genderFinal;
+            TempData["Nationality"] = nationalityFinal;
+            TempData["Address"] = address;
+            TempData["Phone"] = phone;
+            TempData["Email"] = email;
+            return View();
+        }
+    }
+}
