b7b053246e566234577306f2dae38c832c26a284
Brent Lasaca <brent.lasaca@cit.edu>
2023-07-04 09:16:23 +0800

    Add stepper for confirmation in form

---
+++ Basecode.WebApp/Controllers/ConfirmationForm.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class ConfirmationForm : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
