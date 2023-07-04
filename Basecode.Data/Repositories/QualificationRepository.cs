61b7323994b167829c37d857f8b443567f26088d
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-07-04 16:50:41 +0800

    add service and repository for qualifications

---
+++ Basecode.Data/Repositories/QualificationRepository.cs
@@ -0,0 +1,46 @@
+﻿using Basecode.Data.Interfaces;
+using Basecode.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Data.Repositories
+{
+    public class QualificationRepository: BaseRepository, IQualificationRepository
+    {
+        private readonly BasecodeContext _context;
+        public QualificationRepository(IUnitOfWork unitOfWork, BasecodeContext context) : base(unitOfWork)
+        {
+            _context = context;
+        }
+        public IQueryable<Qualification> GetAll()
+        {
+            return this.GetDbSet<Qualification>();
+        }
+
+        public void AddQualification(Qualification qualification)
+        {
+            _context.Qualification.Add(qualification);
+            _context.SaveChanges();
+        }
+
+        public Qualification GetQualificationById(int id)
+        {
+            return _context.Qualification.Find(id);
+        }
+
+        public void UpdateQualification(Qualification qualification)
+        {
+            _context.Qualification.Update(qualification);
+            _context.SaveChanges();
+        }
+
+        public void DeleteQualification(Qualification qualification)
+        {
+            _context.Qualification.Remove(qualification);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Basecode.Services/Interfaces/IQualificationService.cs
@@ -0,0 +1,18 @@
+﻿using Basecode.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Interfaces
+{
+    public interface IQualificationService
+    {
+        List<Qualification> GetQualifications();
+        void Create(Qualification qualification);
+        Qualification GetById(int id);
+        void Update(Qualification qualification);
+        void Delete(Qualification qualification);
+    }
+}
