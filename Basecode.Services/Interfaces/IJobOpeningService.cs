8220e61e09f2b55c53167f180bd8930e850e4af2
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-07-04 14:39:42 +0800

    add documention for job opening service

--- Basecode.Services/Interfaces/IJobOpeningService.cs
+++ Basecode.Services/Interfaces/IJobOpeningService.cs
@@ -1,22 +1,45 @@
 ﻿using Basecode.Data.Models;
 using Basecode.Data.ViewModels;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Basecode.Services.Interfaces
 {
+    /// <summary>
+    /// Defines methods for managing job openings.
+    /// </summary>
     public interface IJobOpeningService
     {
+        /// <summary>
+        /// Gets a list of all job openings.
+        /// </summary>
+        /// <returns>A list of job opening view models.</returns>
         List<JobOpeningViewModel> GetJobs();
+
+        /// <summary>
+        /// Creates a new job opening.
+        /// </summary>
+        /// <param name="jobOpening">The job opening to create.</param>
+        /// <param name="createdBy">The user who created the job opening.</param>
         void Create(JobOpening jobOpening, string createdBy);
 
+        /// <summary>
+        /// Gets a job opening by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the job opening to get.</param>
+        /// <returns>A job opening view model, or null if no such job opening exists.</returns>
         JobOpeningViewModel GetById(int id);
 
+        /// <summary>
+        /// Updates an existing job opening.
+        /// </summary>
+        /// <param name="jobOpening">The job opening to update.</param>
+        /// <param name="updatedBy">The user who updated the job opening.</param>
         void Update(JobOpeningViewModel jobOpening, string updatedBy);
 
+        /// <summary>
+        /// Deletes a job opening.
+        /// </summary>
+        /// <param name="jobOpening">The job opening to delete.</param>
         void Delete(JobOpeningViewModel jobOpening);
     }
 }
