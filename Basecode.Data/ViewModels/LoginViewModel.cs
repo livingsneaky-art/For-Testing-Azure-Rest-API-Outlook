6874441df67388335f2bbe5a73d2aaf70bd5e8dc
Jmark <jaymark.carba@cit.edu>
2023-07-05 22:41:11 +0800

    Add comment

--- Basecode.Data/ViewModels/LoginViewModel.cs
+++ Basecode.Data/ViewModels/LoginViewModel.cs
@@ -8,7 +8,19 @@ namespace Basecode.Data.ViewModels
 {
     public class LoginViewModel
     {
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
         public string Email { get; set; }
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
         public string Password { get; set; }
     }
 }
