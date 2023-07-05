0931620d0485524b27211e8d69c34995b4a3fbba
kuanhh <hannahbenitez62@gmail.com>
2023-07-05 17:03:08 +0800

    Modify documentation for User repository, viewmodel, service

--- Basecode.Data/Repositories/UserRepository.cs
+++ Basecode.Data/Repositories/UserRepository.cs
@@ -11,33 +11,64 @@ namespace Basecode.Data.Repositories
     public class UserRepository : BaseRepository, IUserRepository
     {
         private readonly BasecodeContext _context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserRepository"/> class.
+        /// </summary>
+        /// <param name="unitOfWork">The unit of work.</param>
+        /// <param name="context">The context.</param>
         public UserRepository(IUnitOfWork unitOfWork, BasecodeContext context) : base(unitOfWork)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// Retrieves all users from the User table.
+        /// </summary>
+        /// <returns>
+        /// A queryable collection of objects of type User.
+        /// </returns>
         public IQueryable<User> RetrieveAll()
         {
             return this.GetDbSet<User>();
         }
 
+        /// <summary>
+        /// Adds a new user into the User table.
+        /// </summary>
+        /// <param name="user">Represents the user to be added.</param>
         public void Add(User user)
         {
             _context.User.Add(user);
             _context.SaveChanges();
         }
 
+        /// <summary>
+        /// Retrieves a user from the User table based on the specified ID.
+        /// </summary>
+        /// <param name="id">The ID of the user to retrieve.</param>
+        /// <returns>
+        /// A User object corresponding to the matching ID.
+        /// </returns>
         public User GetById(int id)
         {
             return _context.User.Find(id);
         }
 
+        /// <summary>
+        /// Updates an existing user in the User table.
+        /// </summary>
+        /// <param name="user">Represents the user with updated information.</param>
         public void Update(User user)
         {
             _context.User.Update(user);
             _context.SaveChanges();
         }
 
+        /// <summary>
+        /// Deletes a user from the User table based on the specified ID.
+        /// </summary>
+        /// <param name="id">Represents the ID of the user to be deleted.</param>
         public void Delete(int id)
         {
             var data = _context.User.Find(id);
