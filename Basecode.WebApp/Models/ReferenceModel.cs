befa31abaf1cb992002c7a66c4845c829f68f21a
Brent Lasaca <brent.lasaca@cit.edu>
2023-07-06 10:19:12 +0800

    Pass multiple data from reference to confirmation

---
+++ Basecode.WebApp/Models/ReferenceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Basecode.WebApp.Models
+{
+    public class ReferenceModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+    }
+}
