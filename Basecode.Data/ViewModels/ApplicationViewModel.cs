dc2ba8a19bec6af4b584b3a547f4fe8f816af43c
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2023-07-06 13:16:49 +0800

    Add Documentation for ApplicationViewModel

--- Basecode.Data/ViewModels/ApplicationViewModel.cs
+++ Basecode.Data/ViewModels/ApplicationViewModel.cs
@@ -6,12 +6,34 @@ using System.Threading.Tasks;
 
 namespace Basecode.Data.ViewModels
 {
+    /// <summary>
+    /// View model for application tracker.
+    /// </summary>
     public class ApplicationViewModel
     {
+        /// <summary>
+        /// Gets or sets the ID of the applcation.
+        /// </summary>
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the job opening ID.
+        /// </summary>
         public int JobOpeningId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the applicant ID.
+        /// </summary>
         public int ApplicantId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the application status.
+        /// </summary>
         public string Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the update time of the application.
+        /// </summary>
         public DateTime UpdateTime { get; set; }
     }
 }
