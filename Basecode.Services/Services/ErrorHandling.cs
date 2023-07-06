3d6837f571de44272fca4a4f5cc3525212bb81fe
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-07-07 06:18:31 +0800

    add simplified error logging

--- Basecode.Services/Services/ErrorHandling.cs
+++ Basecode.Services/Services/ErrorHandling.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static Basecode.Services.Services.ErrorHandling;
+﻿using Basecode.Data.ViewModels;
+using System;
 
 namespace Basecode.Services.Services
 {
@@ -12,20 +8,89 @@ namespace Basecode.Services.Services
         public class LogContent
         {
             public string ErrorCode { get; set; } = string.Empty;
-
             public DateTime Time { get; set; }
-
             public string Message { get; set; } = string.Empty;
             public bool Result { get; set; } = false;
+
+            public LogContent CheckJobOpening(JobOpeningViewModel jobOpening)
+            {
+                if (string.IsNullOrEmpty(jobOpening.Title) || jobOpening.Title.Length > 50)
+                {
+                    SetError("400", "Title length is 0 or more than 50 characters.");
+                    return this;
+                }
+
+                if (string.IsNullOrEmpty(jobOpening.EmploymentType))
+                {
+                    SetError("400", "The Employment Type is required but has no value.");
+                    return this;
+                }
+
+                if (string.IsNullOrEmpty(jobOpening.Location))
+                {
+                    SetError("400", "The Location is required but has no value.");
+                    return this;
+                }
+
+                if (string.IsNullOrEmpty(jobOpening.WorkSetup))
+                {
+                    SetError("400", "The Work Setup is required but has no value.");
+                    return this;
+                }
+
+                if (jobOpening.Qualifications == null || jobOpening.Qualifications.Count == 0)
+                {
+                    SetError("400", "The Qualifications list is empty.");
+                    return this;
+                }
+                else
+                {
+                    foreach (var qualification in jobOpening.Qualifications)
+                    {
+                        if (string.IsNullOrEmpty(qualification.Description))
+                        {
+                            SetError("400", "Qualification details are empty.");
+                            return this;
+                        }
+                    }
+                }
+
+                if (jobOpening.Responsibilities == null || jobOpening.Responsibilities.Count == 0)
+                {
+                    SetError("400", "The Responsibilities list is empty.");
+                    return this;
+                }
+                else
+                {
+                    foreach (var responsibility in jobOpening.Responsibilities)
+                    {
+                        if (string.IsNullOrEmpty(responsibility.Description))
+                        {
+                            SetError("400", "Responsibility details are empty.");
+                            return this;
+                        }
+                    }
+                }
+
+                return this;
+            }
+
+            private void SetError(string errorCode, string errorMessage)
+            {
+                Result = true;
+                ErrorCode = errorCode;
+                Message = errorMessage;
+            }
         }
 
         public static string SetLog(LogContent logContent)
         {
-            return "ErrorCode: " + logContent.ErrorCode + ". Message: " + "\"" + logContent.Message + "\"";
+            return $"ErrorCode: {logContent.ErrorCode}. Message: \"{logContent.Message}\"";
         }
+
         public static string DefaultException(string message)
         {
-            return "ErrorCode: " + 500 + ". Message: " + "\"" + message + "\"";
+            return $"ErrorCode: 500. Message: \"{message}\"";
         }
     }
 }
