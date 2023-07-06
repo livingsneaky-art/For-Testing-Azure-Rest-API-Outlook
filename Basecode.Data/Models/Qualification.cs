9af79f1e96be3537aaf55269b836a41ddf7df482
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-07-07 06:42:51 +0800

    fix update controller job opening

--- Basecode.Data/Models/Qualification.cs
+++ Basecode.Data/Models/Qualification.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace Basecode.Data.Models
     {
         public int Id { get; set; }
         public int JobOpeningId { get; set; }
+
+        [Required(ErrorMessage = "The Description is required.")]
         public string Description { get; set; }
     }
 }
