05d2ea7010ef6dfb076c184d76036ed97b61b920
Jmark <jaymark.carba@cit.edu>
2023-07-07 17:25:00 +0800

    Add Login method Tests

--- Basecode.Tests/Controllers/LoginControllerTests.cs
+++ Basecode.Tests/Controllers/LoginControllerTests.cs
@@ -1,8 +1,11 @@
-﻿using Basecode.Services.Interfaces;
+﻿using Basecode.Data.ViewModels;
+using Basecode.Services.Interfaces;
 using Basecode.WebApp.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Extensions.Logging;
 using Moq;
+using NuGet.ContentModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,22 +24,55 @@ namespace Basecode.Tests.Controllers
         }
 
         [Fact]
-        public void Index_VisitLoginPage_ShowScreen()
+        public void Index_VisitLoginPageSuccess_ShowScreen()
         {
-
             //Act
             var result = _controller.Index();
+
             //Assert
             Assert.IsType<ViewResult>(result);
         }
-
-        /*[Fact]
-        public void Index_VisitLoginPage_ErrorScreen()
+        
+        [Fact]
+        public void Login_InputIsValid_RedirectToDashboard()
         {
+            //Arrange
+            var loginVM = new LoginViewModel
+            {
+                Email = "user1@gmail.com",
+                Password = "password1",
+            };
+
             //Act
-            var result = _controller.Index() as StatusCodeResult;
-            //Assert    
-            Assert.Equal(500, result?.StatusCode);
-        }*/
+            var result = _controller.Login(loginVM) as RedirectToActionResult;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", result.ActionName);
+            Assert.Equal("Dashboard", result.ControllerName);
+        }
+
+        [Fact]
+        public void Login_InputIsNotValid_RedirectToIndex()
+        {
+            //Arrange
+            var loginVM = new LoginViewModel
+            {
+                Email = "",
+                Password = "",
+            };
+            _controller.ModelState.AddModelError("Email", "Email is required");
+            _controller.ModelState.AddModelError("Password", "Password is required");
+
+            //Act
+            var result = _controller.Login(loginVM) as RedirectToActionResult;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", result.ActionName);
+        }
+
     }
 }
