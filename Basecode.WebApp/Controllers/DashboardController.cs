24396036d1489350c19fc4cf14f31cd691dcd8ba
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-07-07 12:12:08 +0800

    handle dashboard controller errors

--- Basecode.WebApp/Controllers/DashboardController.cs
+++ Basecode.WebApp/Controllers/DashboardController.cs
@@ -1,22 +1,41 @@
 ﻿using Basecode.Data.Models;
 using Basecode.Services.Interfaces;
+using Basecode.Services.Services;
 using Microsoft.AspNetCore.Mvc;
+using NLog;
 
 namespace Basecode.WebApp.Controllers
 {
     public class DashboardController : Controller
     {
         private readonly IApplicantService _applicantService;
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DashboardController"/> class.
+        /// </summary>
+        /// <param name="applicantService">The applicant service.</param>
         public DashboardController(IApplicantService applicantService)
         {
             _applicantService = applicantService;
         }
 
+        /// <summary>
+        /// Indexes this instance.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
-            List<Applicant> applicants = _applicantService.GetApplicants();
-            return View(applicants);
+            try
+            {
+                List<Applicant> applicants = _applicantService.GetApplicants();
+                return View(applicants);
+            }
+            catch (Exception e)
+            {
+                _logger.Error(ErrorHandling.DefaultException(e.Message));
+                return StatusCode(500, "Something went wrong.");
+            }
         }
     }
 }
