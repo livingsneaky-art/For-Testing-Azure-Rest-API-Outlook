f3357578d05d766cfe93e1ab100f73b83bb4a344
Brent Lasaca <brent.lasaca@cit.edu>
2023-07-07 10:24:28 +0800

    Add unit tests for Public Application Controller

---
+++ Basecode.Tests/Controllers/PublicApplicationControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Basecode.WebApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Tests.Controllers
+{
+    public class PublicApplicationControllerTests
+    {
+        [Fact]
+        public void Index_ReturnsViewResult()
+        {
+            // Arrange
+            var controller = new PublicApplicationController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
