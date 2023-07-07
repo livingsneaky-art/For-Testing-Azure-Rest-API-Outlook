c9900f00b9046618365467c42043378a1670cd46
Brent Lasaca <brent.lasaca@cit.edu>
2023-07-07 15:30:21 +0800

    Update documentation of controllers

--- Basecode.WebApp/Controllers/PublicApplicationController.cs
+++ Basecode.WebApp/Controllers/PublicApplicationController.cs
@@ -4,6 +4,10 @@ namespace Basecode.WebApp.Controllers
 {
     public class PublicApplicationController : Controller
     {
+        /// <summary>
+        /// Starting page of Public Application Form
+        /// </summary>
+        /// <returns>View of Personal Information</returns>
         public IActionResult Index()
         {
             return View();
--- Basecode.WebApp/Controllers/ReferenceController.cs
+++ Basecode.WebApp/Controllers/ReferenceController.cs
@@ -5,7 +5,7 @@ namespace Basecode.WebApp.Controllers
     public class ReferenceController : Controller
     {
         /// <summary>
-        /// Stores data from the inputs of the user for their personal information in the public application form.
+        /// Stores input data from Personal.
         /// </summary>
         /// <param name="firstName"></param>
         /// <param name="middleName"></param>
@@ -14,13 +14,13 @@ namespace Basecode.WebApp.Controllers
         /// <param name="age"></param>
         /// <param name="gender"></param>
         /// <param name="nationality"></param>
-        /// <param name="tempStreet"></param>
-        /// <param name="tempCity"></param>
-        /// <param name="tempProvince"></param>
-        /// <param name="tempZip"></param>
-        /// <param name="tempPhone"></param>
-        /// <param name="tempMail"></param>
-        /// <returns>View of the Character Reference Form Page.</returns>
+        /// <param name="street"></param>
+        /// <param name="city"></param>
+        /// <param name="province"></param>
+        /// <param name="zip"></param>
+        /// <param name="phone"></param>
+        /// <param name="email"></param>
+        /// <returns>View of Character References Form Page</returns>
         [HttpPost]
         public IActionResult Index(string firstName,
                             string middleName,
