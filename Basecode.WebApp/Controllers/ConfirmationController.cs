aac8d145ef4444df84ffcfa8f76a4b63a4c1221c
Brent Lasaca <brent.lasaca@cit.edu>
2023-07-07 15:21:01 +0800

    Revise naming for inputs in views

--- Basecode.WebApp/Controllers/ConfirmationController.cs
+++ Basecode.WebApp/Controllers/ConfirmationController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Net;
 
 namespace Basecode.WebApp.Controllers
 {
@@ -26,21 +27,27 @@ namespace Basecode.WebApp.Controllers
                             string lastName,
                             string birthdate,
                             string age,
-                            string genderFinal,
-                            string nationalityFinal,
-                            string address,
+                            string gender,
+                            string nationality,
+                            string street,
+                            string city,
+                            string province,
+                            string zip,
                             string phone,
                             string email,
                             List<ReferenceModel> references)
         {
             TempData["First Name"] = firstName;
-            TempData["Middle Name"] = " " + middleName;
-            TempData["Last Name"] = " " + lastName;
+            TempData["Middle Name"] = middleName;
+            TempData["Last Name"] = lastName;
             TempData["Birthdate"] = birthdate;
             TempData["Age"] = age;
-            TempData["Gender"] = genderFinal;
-            TempData["Nationality"] = nationalityFinal;
-            TempData["Address"] = address;
+            TempData["Gender"] = gender;
+            TempData["Nationality"] = nationality;
+            TempData["Street"] = street;
+            TempData["City"] = city;
+            TempData["Province"] = province;
+            TempData["Zip"] = zip;
             TempData["Phone"] = phone;
             TempData["Email"] = email;
             string referencesJson = JsonConvert.SerializeObject(references);
