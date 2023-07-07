663b211ffe924d75d84c292c0450dacff9c1a479
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2023-07-07 11:43:01 +0800

    Update ResultView method and Add Documentation

--- Basecode.WebApp/Controllers/TrackerController.cs
+++ Basecode.WebApp/Controllers/TrackerController.cs
@@ -10,16 +10,29 @@ namespace Basecode.WebApp.Controllers
         private readonly IApplicationService _applicationService;
         private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrackerController"/> class.
+        /// </summary>
+        /// <param name="applicationService">The application service.</param>
         public TrackerController(IApplicationService applicationService)
         {
             _applicationService = applicationService;
         }
 
+        /// <summary>
+        /// Indexes this instance.
+        /// </summary>
+        /// <returns>A view of the tracker page</returns>
         public IActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Retrieves an application with the given Id
+        /// </summary>
+        /// <param name="id">The application ID.</param>
+        /// <returns>A view with either the tracker result table or an error message</returns>
         [HttpGet]
         public IActionResult ResultView(Guid id)
         {
@@ -30,7 +43,7 @@ namespace Basecode.WebApp.Controllers
                 {
                     ViewData["ErrorMessage"] = "Application not found.";
                     _logger.Error("Application [" + id + "] not found!");
-                    return NotFound();
+                    return View("Index");
                 }
                 else
                 {
