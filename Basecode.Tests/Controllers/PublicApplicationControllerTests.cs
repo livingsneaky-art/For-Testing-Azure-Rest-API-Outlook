4f87a58f85b48495ae66fe857d5723c6838d83ee
Brent Lasaca <brent.lasaca@cit.edu>
2023-07-07 10:42:19 +0800

    Add unit tests for ReferenceController

--- Basecode.Tests/Controllers/PublicApplicationControllerTests.cs
+++ Basecode.Tests/Controllers/PublicApplicationControllerTests.cs
@@ -1,23 +1,22 @@
 ﻿using Basecode.WebApp.Controllers;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Basecode.Tests.Controllers
 {
     public class PublicApplicationControllerTests
     {
+        private readonly PublicApplicationController _controller;
+
+        public PublicApplicationControllerTests()
+        {
+            _controller = new PublicApplicationController();
+        }
+
         [Fact]
         public void Index_ReturnsViewResult()
         {
-            // Arrange
-            var controller = new PublicApplicationController();
-
             // Act
-            var result = controller.Index();
+            var result = _controller.Index();
 
             // Assert
             Assert.IsType<ViewResult>(result);
