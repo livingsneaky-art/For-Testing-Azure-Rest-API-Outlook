fc5fdcbbeb641429d2082d489763994a263e732d
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-07-07 12:27:45 +0800

    add HomeControllerTests.cs

---
+++ Basecode.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Basecode.Services.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        private readonly Mock<IJobOpeningService> _fakeJobOpeningService;
+        private readonly HomeControllerTests _controller;
+
+
+    }
+}
