62c17c5174b3132d05406d386c2f22e440edf2a0
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-07-12 11:09:52 +0800

    add a method aync Task SendEmail

--- Basecode.Services/Services/EmailService.cs
+++ Basecode.Services/Services/EmailService.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Basecode.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Basecode.Services.Services
 {
-    internal class EmailService
+    public class EmailService : IEmailService
     {
+        public async Task SendEmail(string recipient, string subject, string body)
+        {
+            using (var smtpClient = new SmtpClient("smtp-mail.outlook.com", 587))
+            {
+                smtpClient.EnableSsl = true;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential("hrautomatesystem@outlook.com", "alliance2023");
+
+                using (var mailMessage = new MailMessage())
+                {
+                    mailMessage.From = new MailAddress("hrautomatesystem@outlook.com");
+                    mailMessage.To.Add(recipient);
+                    mailMessage.Subject = subject;
+                    mailMessage.Body = body;
+
+                    await smtpClient.SendMailAsync(mailMessage);
+                }
+            }
+            await Task.CompletedTask;
+        }
     }
 }
